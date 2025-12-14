6baa85e8a3322ed2bf17d99c399f73647c41e30d
smelik81 <artem.smelyanskiy81@gmail.com>
2025-12-15 01:47:55 +0200

    exchange file task-6.ts

--- src/task-6.ts
+++ src/task-6.ts
@@ -6,9 +6,9 @@ const n = getFirstElement<number>([1, 2, 3]); // 1
 const s = getFirstElement<string>(["a", "b", "c"]); // "a"
 const b = getFirstElement<boolean>([true, false, true]); // true
 
-console.log(getFirstElement([1, 2, 3]));
-console.log(getFirstElement(["a", "b", "c"]));
-console.log(getFirstElement([true, false, false]));
+console.log(getFirstElement<number>([1, 2, 3]));
+console.log(getFirstElement<string>(["a", "b", "c"]));
+console.log(getFirstElement<boolean>([true, false, false]));
 
 console.log(n);
 console.log(s);
